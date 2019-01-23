d39cbb33ef3188792759b350b278d430aa965235
Soutine <jsoutine@gmail.com>
2019-01-23 13:45:10 +0100

    more on l05

--- src/com/company/l05_methods/Task4.java
+++ src/com/company/l05_methods/Task4.java
@@ -5,10 +5,14 @@ public class Task4 {
 
         Task4 myApp = new Task4();
         myApp.printTwoParams(5,10);
+        myApp.printDoubles(0.5,5.7,100.8);
 
     }
     private void printTwoParams(int a, int b){
         System.out.println("a = " + a);
         System.out.println("b = " + b);
+    }
+    private void printDoubles(double a, double b, double c){
+        System.out.printf("%.2f, %.2f, %.2f %n%f%n%f%n%f%n", a, b, c, a, b, c );
     }
 }
